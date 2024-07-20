06eec4a477a30f8543a09bdff14a02f181d0bec8
ribhavsingla <ribhavsingla2166@gmail.com>
2024-07-20 16:47:52 +0530

    zodValidated_admin_log_status

--- backend/src/Route/Admin/log.ts
+++ backend/src/Route/Admin/log.ts
@@ -1,10 +1,18 @@
 import express from 'express'
 import { Logs } from '../../Models/Logs'
 import { adminAuth } from '../../Middleware/admin';
+import { adminLogStatusSchema } from '../../zod';
 
 export const logRouter = express.Router()
 
 logRouter.put('/updateStatus/:id',adminAuth,async(req,res)=>{
+    const status = req.body.status
+    const {success,error}  =adminLogStatusSchema.safeParse({status})
+
+    if(error){
+        console.log('error occured while parsing admin log status: ',error)
+        return res.status(400).json({error:'Invalid Inputs'})
+    }
     
     const logId = req.params.id
     try {
--- backend/src/zod/index.ts
+++ backend/src/zod/index.ts
@@ -14,3 +14,7 @@ export const userPasswordSchema = z.object({
     oldPassword: z.string().optional(),
     password: z.string().trim().regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{6,}$/, 'Password must contain at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character') 
 })
+
+export const adminLogStatusSchema = z.object({
+    status: z.enum(['Accepted', 'Rejected'])
+})
